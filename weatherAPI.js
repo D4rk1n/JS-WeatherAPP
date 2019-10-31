462a753931a6d1f051f76c1f4c2526aa35ab9c02
Darkin <darkinb255@gmail.com>
2019-10-31 15:14:29 +0200

    getConditions

--- weatherAPI.js
+++ weatherAPI.js
@@ -1,20 +1,27 @@
 const apiKey = 'uY4ljA9H86LqKrqoIFJlIffsPRvahhKw';
 
 
-
-
-
-const getCity = async (city)=> {
-    const url = `http://dataservice.accuweather.com/locations/v1/cities/search?apikey=${apiKey}&q=${city}`;
-    const response =  await fetch(url);
+const getConditions = async (location) =>{
+    const url = `http://dataservice.accuweather.com/currentconditions/v1/${location}`;
+    const q = `?apikey=${apiKey}`;
+    const response =  await fetch(url+q);
     const data = await response.json();
     return data[0];
 }
-let d = null ;
-let c = getCity('giza').then(data =>{
-console.log(data);
-d= data;
-console.log(d);
+
+const getCity = async (city)=> {
+    const url = 'http://dataservice.accuweather.com/locations/v1/cities/search';
+    const q = `?apikey=${apiKey}&q=${city}`;
+    const response =  await fetch(url+q);
+    const data = await response.json();
+    return data[0];
+}
+
+
+let c = getCity('cairo').then(city =>{
+return getConditions(city.Key);
+}).then(weather => {
+    console.log(weather)
 });
 
 
