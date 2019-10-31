44ca88279252eb6cf045f6f9564425cd2f29e857
Darkin <darkinb255@gmail.com>
2019-10-31 19:23:25 +0200

    Done / no cond.

---
+++ app.js
@@ -0,0 +1,45 @@
+const form = document.querySelector(".change-city");
+const card = document.querySelector(".card");
+const details = document.querySelector(".details");
+const timeImg = document.querySelector("img.time");
+console.log(timeImg);
+
+
+const updateCity = async cityName => {
+    const city = await getCity(cityName);
+    const conditions = await getConditions(city.Key);
+    return { city  , conditions}
+}
+
+function updateUI(data) {
+    const{city , conditions} = data;
+    console.log(city,conditions);
+    details.innerHTML = `
+    <h5 class="my-3" name="city">${city.EnglishName}</h5>
+    <div class="my-3">${conditions.WeatherText}</div>
+    <div class="display-4 my-4">
+      <span name="temp">${conditions.Temperature.Metric.Value}</span>
+      <span>&deg;C</span>
+    </div>`;
+    if(conditions.IsDayTime)
+    {
+        timeImg.setAttribute("src","img/Day.svg");
+        console.log("day");
+    }
+    else {
+        timeImg.setAttribute("src","img/Night.svg");
+    }
+
+}
+
+
+form.addEventListener("submit" , (e)=>{
+    e.preventDefault();
+    updateCity(form.city.value.trim())
+    .then(data => updateUI(data))
+    .catch(err => console.log(err.message));
+    form.reset(); 
+})
+form.addEventListener("keyup" , e => {
+    //console.log(form.city.value.trim());
+})
--- weatherAPI.js
+++ weatherAPI.js
@@ -1,13 +1,7 @@
 const apiKey = 'uY4ljA9H86LqKrqoIFJlIffsPRvahhKw';
 
 
-const getConditions = async (location) =>{
-    const url = `http://dataservice.accuweather.com/currentconditions/v1/${location}`;
-    const q = `?apikey=${apiKey}`;
-    const response =  await fetch(url+q);
-    const data = await response.json();
-    return data[0];
-}
+
 
 const getCity = async (city)=> {
     const url = 'http://dataservice.accuweather.com/locations/v1/cities/search';
@@ -17,11 +11,13 @@ const getCity = async (city)=> {
     return data[0];
 }
 
+const getConditions = async (location) =>{
+    const url = `http://dataservice.accuweather.com/currentconditions/v1/${location}`;
+    const q = `?apikey=${apiKey}`;
+    const response =  await fetch(url+q);
+    const data = await response.json();
+    return data[0];
+}
 
-let c = getCity('cairo').then(city =>{
-return getConditions(city.Key);
-}).then(weather => {
-    console.log(weather)
-});
 
 
